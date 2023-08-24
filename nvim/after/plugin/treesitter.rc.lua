38b9c733f27aa1e37e684cf253c261573e578a2c
Kenley <3004590+kenleytomlin@users.noreply.github.com>
2023-08-24 14:06:09 +0100

    chore: update plugins to latest

--- nvim/after/plugin/treesitter.rc.lua
+++ nvim/after/plugin/treesitter.rc.lua
@@ -2,30 +2,14 @@ local status, ts = pcall(require, 'nvim-treesitter.configs')
 if (not status) then return end
 
 ts.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {}
-  },
-  ensure_installed = {
-    'tsx',
-    'toml',
-    'lua',
-    'json',
-    'yaml',
-    'css',
-    'html',
-    'elixir',
-    'heex',
-    'eex'
-  },
-  autotag = {
-    enable = true
-  }
+    highlight = {enable = true, disable = {}},
+    indent = {enable = true, disable = {}},
+    ensure_installed = {
+        'tsx', 'toml', 'lua', 'json', 'yaml', 'css', 'html', 'elixir', 'heex',
+        'eex', 'markdown', 'markdown_inline'
+    },
+    autotag = {enable = true}
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+local parser_config = require"nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = {"javascript", "typescript.tsx"}
--- nvim/lua/kenleytomlin/plugins.lua
+++ nvim/lua/kenleytomlin/plugins.lua
@@ -1,57 +1,49 @@
 local status, packer = pcall(require, 'packer')
 
 if (not status) then
-  print('Packer is not installed')
-  return
+    print('Packer is not installed')
+    return
 end
 
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  } -- Solarized colors
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  } -- Status line
-  use 'nvim-lua/plenary.nvim' -- Common utilities
-  use 'onsails/lspkind-nvim' -- vscode like pictograms
-  use 'norcalli/nvim-colorizer.lua'
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-  use { 'nvim-telescope/telescope-file-browser.nvim' }
-  use {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-  }
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use {
-    'glepnir/lspsaga.nvim', commit = 'b7b4777369b441341b2dcd45c738ea4167c11c9e'
-  }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-  use 'neovim/nvim-lspconfig' -- LSP
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/nvim-cmp' -- Completion
-  use 'hrsh7th/cmp-nvim-lsp'
+    use 'wbthomason/packer.nvim'
+    use {'svrana/neosolarized.nvim', requires = {'tjdevries/colorbuddy.nvim'}} -- Solarized colors
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    } -- Status line
+    use 'nvim-lua/plenary.nvim' -- Common utilities
+    use 'onsails/lspkind-nvim' -- vscode like pictograms
+    use 'norcalli/nvim-colorizer.lua'
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        requires = {{'nvim-lua/plenary.nvim'}}
+    }
+    use {'nvim-telescope/telescope-file-browser.nvim'}
+    use {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    }
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+    use 'jose-elias-alvarez/null-ls.nvim'
+    use {'nvimdev/lspsaga.nvim', after = 'nvim-lspconfig'}
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use 'neovim/nvim-lspconfig' -- LSP
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/nvim-cmp' -- Completion
+    use 'hrsh7th/cmp-nvim-lsp'
 
-  use 'L3MON4D3/LuaSnip' -- luasnip completion
+    use 'L3MON4D3/LuaSnip' -- luasnip completion
 
-  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
+    use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
 
-  use 'folke/tokyonight.nvim'
-  use 'tpope/vim-surround'
+    use 'folke/tokyonight.nvim'
+    use 'tpope/vim-surround'
 
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
+    use 'windwp/nvim-autopairs'
+    use 'windwp/nvim-ts-autotag'
 end)
