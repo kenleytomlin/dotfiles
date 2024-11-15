67531074f0aafebc41263dbce47c0909f5dab437
kenleytomlin <3004590+kenleytomlin@users.noreply.github.com>
2024-11-15 10:26:09 +0000

    fix: ensure snippets work as expected

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,6 +4,7 @@ return {
 		version = false, -- last release is way too old
 		event = "InsertEnter",
 		dependencies = {
+			"L3MON4D3/LuaSnip",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
@@ -18,8 +19,7 @@ return {
 			local has_words_before = function()
 				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
-						== nil
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
 			return {
 				completion = { completeopt = "menu,menuone,noinsert" },
@@ -32,7 +32,7 @@ return {
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
+						elseif luasnip.expand_or_jump() then
 							luasnip.expand_or_jump()
 						elseif has_words_before() then
 							cmp.complete()
